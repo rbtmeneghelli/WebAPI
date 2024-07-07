ab7e9582ebc7c80a44ef15153fad033e3515d7b6
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-07-07 18:12:27 -0300

    Versão 29 - Configurado as varivaveis de ambiente do projeto WebAPI
    Commit (Versão 29 - Configurado as varivaveis de ambiente do projeto WebAPI)

--- DEV.bat
+++ DEV.bat
@@ -1,7 +1,9 @@
 C:
 
-setx WEBAPI_CONNECTIONSTRING_SQL ""
-setx WEBAPI_CONNECTIONSTRING_DOCKER ""
-setx WEBAPI_CONNECTIONSTRING_RABBITMQ ""
+setx WebAPI_Docker "server=sqlData;database=DefaultAPI;Trusted_Connection=True;MultipleActiveResultSets=true;trustservercertificate=true;"
+setx WebAPI_KissLog "{"OrganizationId": "XXXX", "ApplicationId": "XXXX", "ApiUrl": "https://api.kisslog.net"}";
+setx WebAPI_Logs "Server=.\\SQLEXPRESS;Database=DefaultAPI_Logs;User Id=sa;Password=#XXXX;MultipleActiveResultSets=true;trustservercertificate=true;"
+setx WebAPI_Sql "Server=.\\SQLEXPRESS;Database=DefaultAPI;User Id=sa;Password=#XXXX;MultipleActiveResultSets=true;trustservercertificate=true;"
+setx WebAPI_Cors "["http://localhost:4200", "https://localhost:4200", "https://api.kisslog.net/", "http://api.kisslog.net/"]"
 
 pause
