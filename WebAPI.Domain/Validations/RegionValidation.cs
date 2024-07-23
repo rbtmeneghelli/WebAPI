63c04b28e4cf120c023ecd3bced9ac4ffa64a31d
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-07-23 13:49:25 -0300

    Versão 37 - Novas config do fluentValidation
    Commit (Versão 37 - Novas config do fluentValidation)

--- WebAPI.Domain/Validations/RegionValidation.cs
+++ WebAPI.Domain/Validations/RegionValidation.cs
@@ -10,21 +10,29 @@ public class RegionValidation : AbstractValidator<Region>
 {
     public RegionValidation()
     {
+        // Id deve ser maior que 0
         RuleFor(x => x.Id)
             .GreaterThan(0)
-            .WithMessage("Id é inválido.");
+            .WithMessage("O valor '{PropertyValue}' para '{PropertyName}' é inválido.");
 
+        // Nome - MinLength 8, MaxLength 100
         RuleFor(x => x.Name)
+            .MinimumLength(8)
+            .WithMessage("O '{PropertyName}' deve ter no mínimo {MinLength} caracteres.")
+            .MaximumLength(100)
+            .WithMessage("O '{PropertyName}' deve ter no máximo {MaxLength} caracteres.");
+
+        RuleFor(x => x.Initials)
             .MinimumLength(2)
-            .WithMessage("O 'Nome' deve ter no mínimo 8 caracteres.")
-            .MaximumLength(10)
-            .WithMessage("O 'Nome' deve ter no máximo 10 caracteres.");
+            .WithMessage("O '{PropertyName}' deve ter no entre {MinLength} e {MaxLength} caracteres, porém foi inserido {TotalLength} caracteres")
+            .MaximumLength(3)
+            .WithMessage("O '{PropertyName}' deve ter no entre {MinLength} e {MaxLength} caracteres, porém foi inserido {TotalLength} caracteres");
 
         //Tratamento com Coleção
         RuleFor(x => x.States)
             .NotNull()
-            .WithMessage("A coleção 'Cursos' deve ter no mínimo 5 cursos.")
+            .WithMessage("A coleção '{PropertyName}' deve ter no mínimo 5 cursos.")
             .Must((region, states, builder) => states.Count() >= 5)
-            .WithMessage("A coleção 'Cursos' deve ter no mínimo 5 cursos.");
+            .WithMessage("A coleção '{PropertyName}' deve ter no mínimo 5 cursos.");
     }
 }
