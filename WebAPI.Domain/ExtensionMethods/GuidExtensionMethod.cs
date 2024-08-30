31d89cba6ffe0751cbacda6669121fa27b41d2e7
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-08-30 13:32:59 -0300

    Versão 54 - Criado metodos de extensão para GUID
    Commit (Versão 54 - Criado metodos de extensão para GUID)

---
+++ WebAPI.Domain/ExtensionMethods/GuidExtensionMethod.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPI.Domain.ExtensionMethods;
+
+/// <summary>
+/// Formatos do GUID
+/// D: 32 dígitos, mas com os hífens. Esse é o padrão;
+/// N: 32 dígitos, sem outros símbolos; (Ideal para usar em nome de arquivos)
+/// B: Usa os hífens e a string é envolta entre chaves
+/// P: semelhante a B, mas com parênteses em vez de chaves
+/// X:  Usa a representação hexadecimal do guid.
+/// </summary>
+public static class GuidExtensionMethod
+{
+    public static string GetGuidDigits(string typeGuid) => Guid.NewGuid().ToString(typeGuid);
+    public static Guid GetGuidDigitsByString(this string text) => Guid.Parse(text);
+}
