b5ca723dab03b12c6ae3e850ad3468b262178897
Roberto Meneghelli <r.grigoragi@avivatec.com.br>
2024-12-02 16:05:53 -0300

    Versão 114 - Adicionado metodo utilizando a palavra chave params do NET 9
    Commit (Versão 114 - Adicionado metodo utilizando a palavra chave params do NET 9)

--- WebAPI.Domain/ExtensionMethods/ListExtensionMethod.cs
+++ WebAPI.Domain/ExtensionMethods/ListExtensionMethod.cs
@@ -2,110 +2,129 @@
 using WebAPI.Domain.Models;
 using System.Data;
 
-namespace WebAPI.Domain.ExtensionMethods
+namespace WebAPI.Domain.ExtensionMethods;
+
+public class ListExtensionMethod
 {
-    public class ListExtensionMethod
+    public Queue<T> ConvertListToQueue<T>(IEnumerable<T> list) => new Queue<T>(list);
+
+    /// <summary>
+    /// Faz a combinação dos itens da list1 e list2, em uma unica lista
+    /// Esse procedimento a partir do NET 12 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list1"></param>
+    /// <param name="list2"></param>
+    /// <returns></returns>
+    public IEnumerable<T> CombineLists<T>(IEnumerable<T> list1, IEnumerable<T> list2) => [.. list1, .. list2];
+
+    public T[] GetAllElements<T>(T[] list) => list[Range.All];
+
+    public T[] GetAllElementsNFirsts<T>(T[] list, int numberOfElements) => list[..numberOfElements];
+
+    public T[] GetAllElementsNLasts<T>(T[] list, int numberOfElements) => list[..^numberOfElements];
+
+    public T[] GetAllElementsAfterNFirst<T>(T[] list, int numberOfElements) => list[numberOfElements..];
+
+    public T[] GetAllElementsAfterNLast<T>(T[] list, int numberOfElements) => list[^numberOfElements..];
+
+    public T[] GetReverseArray<T>(T[] array)
     {
-        public Queue<T> ConvertListToQueue<T>(IEnumerable<T> list) => new Queue<T>(list);
-
-        /// <summary>
-        /// Faz a combinação dos itens da list1 e list2, em uma unica lista
-        /// Esse procedimento a partir do NET 12 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list1"></param>
-        /// <param name="list2"></param>
-        /// <returns></returns>
-        public IEnumerable<T> CombineLists<T>(IEnumerable<T> list1, IEnumerable<T> list2) => [.. list1, .. list2];
-
-        public T[] GetAllElements<T>(T[] list) => list[Range.All];
-
-        public T[] GetAllElementsNFirsts<T>(T[] list, int numberOfElements) => list[..numberOfElements];
-
-        public T[] GetAllElementsNLasts<T>(T[] list, int numberOfElements) => list[..^numberOfElements];
-
-        public T[] GetAllElementsAfterNFirst<T>(T[] list, int numberOfElements) => list[numberOfElements..];
-
-        public T[] GetAllElementsAfterNLast<T>(T[] list, int numberOfElements) => list[^numberOfElements..];
-
-        public T[] GetReverseArray<T>(T[] array)
-        {
-            Array.Reverse(array);
-            return array;
-        }
-
-        public IEnumerable<T> GetReverseList<T>(IEnumerable<T> list) => Enumerable.Reverse(list);
-
-        public IEnumerable<T> ReturnListOrEmptyList<T>(IEnumerable<T> source) => source is not null ? source : Enumerable.Empty<T>();
-
-        public IEnumerable<DropDownList> ConvertEnumToList<T>() where T : Enum
-        {
-            IEnumerable<DropDownList> list = Enum.GetValues(typeof(EnumSystem))
-              .Cast<EnumSystem>()
-              .Select(x => new DropDownList
-              {
-                  Id = ((long)x),
-                  Description = x.ToString()
-              });
-
-            return list;
-        }
-
-        public IEnumerable<string> CloneList(List<string> list)
-        {
-            return list.GetRange(0, list.Count);
-        }
-
-        public IEnumerable<T> ConvertToList<T>(DataTable dt)
-        {
-            var columnNames = dt.Columns.Cast<DataColumn>().Select(c => c.ColumnName.ToLower()).ToList();
-            var properties = typeof(T).GetProperties();
-            return dt.AsEnumerable().Select(row =>
-            {
-                var objT = Activator.CreateInstance<T>();
-                foreach (var pro in properties)
-                {
-                    if (columnNames.Contains(pro.Name.ToLower()))
-                    {
-                        try
-                        {
-                            pro.SetValue(objT, row[pro.Name]);
-                        }
-                        catch (Exception ex)
-                        { throw new Exception(ex.Message, ex.InnerException); }
-                    }
-                }
-                return objT;
-            }).ToList();
-        }
-
-        public bool CompareLists(IEnumerable<int> listNumber, IEnumerable<int> listNumbers)
-        {
-            return listNumber.Except(listNumbers).Any() &&
-            listNumbers.Except(listNumber).Any();
-        }
-
-        public bool CompareAllLinq(IEnumerable<int> numeros1, IEnumerable<int> numeros2)
-        {
-            bool isEqual = numeros1.All(numeros2.Contains)
-                           && numeros2.All(numeros1.Contains);
-
-            return isEqual;
-        }
-
-        /// <summary>
-        /// Esse metodo funciona a partir do NET 9, tem performance melhor do que utilizar CONTAINS
-        /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        //public bool ExistItemOnList(IEnumerable<string> list)
-        //{
-        //    SearchValues<string> searchValues = SearchValues.Create(
-        //        ["Item1", "Item2", "Item3"],
-        //        StringComparison.OrdinalIgnoreCase
-        //    );
-
-        //    return list.Where(p => searchValues.Contains(p)).Any();
-        //}
+        Array.Reverse(array);
+        return array;
     }
+
+    public IEnumerable<T> GetReverseList<T>(IEnumerable<T> list) => Enumerable.Reverse(list);
+
+    public IEnumerable<T> ReturnListOrEmptyList<T>(IEnumerable<T> source) => source is not null ? source : Enumerable.Empty<T>();
+
+    public IEnumerable<DropDownList> ConvertEnumToList<T>() where T : Enum
+    {
+        IEnumerable<DropDownList> list = Enum.GetValues(typeof(EnumSystem))
+          .Cast<EnumSystem>()
+          .Select(x => new DropDownList
+          {
+              Id = ((long)x),
+              Description = x.ToString()
+          });
+
+        return list;
+    }
+
+    public IEnumerable<string> CloneList(List<string> list)
+    {
+        return list.GetRange(0, list.Count);
+    }
+
+    public IEnumerable<T> ConvertToList<T>(DataTable dt)
+    {
+        var columnNames = dt.Columns.Cast<DataColumn>().Select(c => c.ColumnName.ToLower()).ToList();
+        var properties = typeof(T).GetProperties();
+        return dt.AsEnumerable().Select(row =>
+        {
+            var objT = Activator.CreateInstance<T>();
+            foreach (var pro in properties)
+            {
+                if (columnNames.Contains(pro.Name.ToLower()))
+                {
+                    try
+                    {
+                        pro.SetValue(objT, row[pro.Name]);
+                    }
+                    catch (Exception ex)
+                    { throw new Exception(ex.Message, ex.InnerException); }
+                }
+            }
+            return objT;
+        }).ToList();
+    }
+
+    public bool CompareLists(IEnumerable<int> listNumber, IEnumerable<int> listNumbers)
+    {
+        return listNumber.Except(listNumbers).Any() &&
+        listNumbers.Except(listNumber).Any();
+    }
+
+    public bool CompareAllLinq(IEnumerable<int> numeros1, IEnumerable<int> numeros2)
+    {
+        bool isEqual = numeros1.All(numeros2.Contains)
+                       && numeros2.All(numeros1.Contains);
+
+        return isEqual;
+    }
+
+    /// <summary>
+    /// Esse metodo funciona a partir do NET 9, tem performance melhor do que utilizar CONTAINS
+    /// </summary>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    //public bool ExistItemOnList(IEnumerable<string> list)
+    //{
+    //    SearchValues<string> searchValues = SearchValues.Create(
+    //        ["Item1", "Item2", "Item3"],
+    //        StringComparison.OrdinalIgnoreCase
+    //    );
+
+    //    return list.Where(p => searchValues.Contains(p)).Any();
+    //}
+
+    #region Trabalhando com a palavra chave params
+
+    public int GetTotalElementsFromParams(params int[] numeros)
+    {
+        // So funciona com Arrays
+        // GetTotalElementsFromParams(1, 2, 3, 4); 
+        return numeros.Length;
+    }
+
+    //public int GetTotalElementsFromNET9(params IEnumerable numeros)
+    //{
+    //    // Funciona com qualquer tipo de coleção. (No caso desse metodo não é necessario efetuar a conversão explicita do parametro do metodo)
+    //    // ContadorNumeros(new List<int> { 1, 2, 3 });
+    //    // ou ContadorNumeros(new int[] { 1, 2, 3 });
+    //    // ou ContadorNumeros(Enumerable.Range(1,3));
+    //    return numeros.Length;
+    //}
+
+    #endregion
 }
