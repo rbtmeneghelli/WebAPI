cfa99f89d9deb364cf860e39193dcf642558f0dc
Roberto Meneghelli <36852307+rbtmeneghelli@users.noreply.github.com>
2025-02-14 21:34:10 -0300

    Exemplo de teste unitario com NUnit

---
+++ TestsWebAPI/Controllers/V1/NUnitControllerTest.cs
@@ -0,0 +1,38 @@
+﻿namespace TestsWebAPI.Controllers.V1;
+
+// Exemplo de teste unitario com NUnit (Verificar se a injeção de dependencia funciona ou se precisa do BuilderService)
+[TestFixture]
+public sealed class NUnitControllerTest
+{
+    private Mock<IDataRepository> _mockDataRepository;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockDataRepository = new Mock<IDataRepository>();
+    }
+
+    [TestCase("", "73990324000199")]
+    [TestCase("", "73990324000199")]
+    public async Task GetAllData_ShouldReturnPagedListResponse_WhenTakersExist(string name, string cpfCnpj)
+    {
+        // Arrange
+        var dataList = new List<Data>
+        {
+            new Data { Id = 1, Name = "Data 1",  Enabled = true,  CpfCnpj = "19013280072" },
+            new Data { Id = 2, Name = "Data 2",  Enabled = false, CpfCnpj = "29399711021" },
+            new Data { Id = 3, Name = "Data 3",  Enabled = false, CpfCnpj = "63383005000160" },
+            new Data { Id = 4, Name = "Data 4",  Enabled = false, CpfCnpj = "73990324000198" }
+        };
+
+        // Act
+        _mockDataRepository.Setup(repo => repo.GetAllData(null, null, 0, int.MaxValue)).ReturnsAsync(List<Data>(dataList, 0, 10));
+
+        var mockResult = await _mockDataRepository.Object.GetAllData(null, null, 0, int.MaxValue);
+
+        // Assert
+        Assert.That(mockResult.Count, Is.EqualTo(4));
+    }
+}
+
+
