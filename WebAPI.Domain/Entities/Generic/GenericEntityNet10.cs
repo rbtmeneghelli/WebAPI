567bd70f55be352812c7a8f66424a7e9a18675f5
Roberto Meneghelli <roberto.grigoragi@prestador.bmgseguros.com.br>
2025-03-05 16:36:56 -0300

    Versão 140 - Exemplo da nova funcionalidade do NET 10 com C#14
    Commit (Versão 140 - Exemplo da nova funcionalidade do NET 10 com C#14)

---
+++ WebAPI.Domain/Entities/Generic/GenericEntityNet10.cs
@@ -0,0 +1,41 @@
+﻿using WebAPI.Domain.Constants;
+using WebAPI.Domain.ExtensionMethods;
+
+namespace WebAPI.Domain.Entities.Generic;
+
+/// <summary>
+/// A partir do NET 10 com C# 14 não será necessario criar uma propriedade privada, esse processo será substituido pela palavra chave field.
+/// </summary>
+public abstract class GenericEntityNet10
+{
+    //public long? Id
+    //{
+    //    get
+    //    {
+    //        return field;
+    //    }
+    //    set
+    //    {
+    //        field = value.HasValue ? value > 0 ? value : null : null;
+    //    }
+    //}
+
+    //public DateTime? CreateDate
+    //{
+    //    get
+    //    {
+    //        return field;
+    //    }
+    //    set
+    //    {
+    //        field = _id.HasValue ? value : DateOnlyExtensionMethods.GetDateTimeNowFromBrazil();
+    //    }
+    //}
+
+    public DateTime? UpdateDate { get; set; }
+
+    public bool Status { get; set; }
+
+    public string GetStatusDescription() => Status ? FixConstants.STATUS_ACTIVE : FixConstants.STATUS_INACTIVE;
+    public DateTime GetNewUpdateDate() => DateOnlyExtensionMethods.GetDateTimeNowFromBrazil();
+}
