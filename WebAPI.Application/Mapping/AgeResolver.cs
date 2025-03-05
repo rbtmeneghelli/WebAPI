c8dae6affeec62d64f1f3ba7cc35942b06ff49ea
Roberto Meneghelli <36852307+rbtmeneghelli@users.noreply.github.com>
2025-03-05 18:45:25 -0300

    Update AgeResolver.cs

--- WebAPI.Application/Mapping/AgeResolver.cs
+++ WebAPI.Application/Mapping/AgeResolver.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using System;
 
-public class AgeResolver : IValueResolver<Employee, EmployeeDTO, int>
+public class AgeResolver : IValueResolver<Employee, EmployeeResponseDTO, int>
 {
   public int Resolve(Employee source, EmployeeDTO destination, int destMember, ResolutionContext context)
   {
