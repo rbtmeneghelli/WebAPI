129f93b962993d1d91a5de3784d3506bc9f70a62
Roberto Meneghelli <r.grigoragi@avivatec.com.br>
2025-07-10 15:33:36 -0300

    Versão 163 - Ajustado GlobalExceptionHandler
    Commit (Versão 163 - Ajustado GlobalExceptionHandler)

--- WebAPI.Application/Factory/ProblemDetailsFactory.cs
+++ WebAPI.Application/Factory/ProblemDetailsFactory.cs
@@ -1,4 +1,7 @@
-﻿using WebAPI.Domain.Interfaces.Factory;
+﻿using Newtonsoft.Json;
+using SendGrid.Helpers.Errors.Model;
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Domain.Interfaces.Factory;
 using WebAPI.Domain.Models.Factory.ProblemDetails;
 
 namespace WebAPI.Application.Factory;
@@ -9,10 +12,17 @@ public sealed class ProblemDetailsFactory : IProblemDetailsFactory
     {
         return exception switch
         {
-            ArgumentNullException => new BadRequestProblemDetails(),
-            ApplicationException => new InternalErrorProblemDetails(),
-            UnauthorizedAccessException => new TokenProblemDetails(),
-            _ => throw new Exception()
+            ArgumentException or ArgumentNullException or FormatException or
+            InvalidCastException or KeyNotFoundException or ValidationException or
+            ValidationException or BadRequestException or Newtonsoft.Json.JsonException or JsonReaderException
+            => new BadRequestProblemDetails(),
+            UnauthorizedAccessException or ForbiddenException
+            => new TokenProblemDetails(),
+            NullReferenceException or InvalidOperationException or IndexOutOfRangeException or DivideByZeroException or
+            IOException or FileNotFoundException or TimeoutException or OutOfMemoryException or
+            StackOverflowException or TaskCanceledException 
+            => new InternalErrorProblemDetails(),
+            _ => new InternalErrorProblemDetails()
         };
     }
 }
